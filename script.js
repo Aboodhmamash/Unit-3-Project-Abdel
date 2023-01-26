e761ced72e5a87dfd58b35b7495d2354597ab91b
Aboodhmamash <123671016+Aboodhmamash@users.noreply.github.com>
2023-01-26 11:34:45 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+// let storyOpeing = document.querySelector(".story-Opeing");
+// let buttons = document.querySelector(".buttons");
+//let storypart2yes = document.querySelector(".storypart2yes");
+// let storypart2no = document.querySelector(".storypart2no");
+// let storypart3yes = document.querySelector(".storypart3yes");
+// let storypart3no = document.querySelector(".storypart3no");
+// let img = document.querySelector(".img");
+// let paragraph = document.querySelector(".paragraph");
+
+let op1 = document.querySelector(".Option-1");
+let op2 = document.querySelector(".Option-2");
+
+let op1screen = document.querySelector(".option-one-screen");
+
+
+op1.onclick = function() {
+    op1screen.style.display = "block";
+};
+
+
+ let op2screen = document.querySelector(".option-two-screen");
+
+op2.onclick = function() {
+    op2screen.style.display = "block";
+};
+
